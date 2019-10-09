d4fb49f3d273abaee3824c40f02530fbcd4533c5
Hudie <ludanfeng@bestminr.com>
2019-10-09 18:13:49 +0800

    Optimized data consumer class to handle more than one data source

--- mdconsumer.py
+++ mdconsumer.py
@@ -4,6 +4,7 @@
 from tornado.platform.asyncio import AsyncIOMainLoop
 from service.base import ServiceState, ServiceBase, start_service
 import zmq.asyncio
+import asyncio
 import json
 
 
@@ -21,9 +22,21 @@ class DeribitMDConsumer(ServiceBase):
         self.msgclient.connect('tcp://localhost:9000')
         self.msgclient.setsockopt_string(zmq.SUBSCRIBE, '')
 
+        self.msgclient2 = self.ctx.socket(zmq.SUB)
+        self.msgclient2.connect('tcp://localhost:9001')
+        self.msgclient2.setsockopt_string(zmq.SUBSCRIBE, '')
+        
+    # deal with data source 1
     async def sub_msg(self):
         while self.state == ServiceState.started:
             msg = await self.msgclient.recv_string()
+            # deal with the coming msg
+            print(msg)
+
+    # deal with data source 2
+    async def sub_msg2(self):
+        while self.state == ServiceState.started:
+            msg = await self.msgclient2.recv_string()
             # deal with the coming msg
             print(msg)
 
@@ -32,7 +45,8 @@ class DeribitMDConsumer(ServiceBase):
             self.logger.error('tried to run service, but state is %s' % self.state)
         else:
             self.state = ServiceState.started
-            await self.sub_msg()
+            # run sub_msg & sub_msg2 concurrently
+            await asyncio.gather(self.sub_msg(), self.sub_msg2())
 
 
     
