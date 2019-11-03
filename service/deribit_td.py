7953803b609877a6875d2f59ae45524a23796b2c
Hudie <ludanfeng@bestminr.com>
2019-11-03 16:36:54 +0800

    Tuned TD structure to deal with multiple accounts

--- service/deribit_td.py
+++ service/deribit_td.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from crypto_foundation.api.deribit_parser import parse_deribit_trade, parse_deribit_quote, parse_deribit_order_book, parse_deribit_instrument
+from crypto_foundation.common.constant import Ecn
 from base import ServiceState, ServiceBase, start_service
 import zmq.asyncio
 import asyncio
@@ -13,7 +14,7 @@ import time
 
 orders = {}
 
-DERIBIT_EXCHANGE_ID = 101
+DERIBIT_EXCHANGE_ID = Ecn.deribit
 DERIBIT_CLIENT_ID = "8VP1NV2u"
 DERIBIT_CLIENT_SECRET = "LEnJgX-u5LpROjpOKGVcY_NRbC_nByBIOg-mCflwzMg"
 SYMBOL = 'BTC'
@@ -74,20 +75,15 @@ class DeribitMD(ServiceBase):
         self.repserver = self.ctx.socket(zmq.REP)
         self.repserver.bind('tcp://*:9020')
 
-    async def pub_msg(self):
+    async def pub_msg(self, accountid):
         # get tx data from exchange socket, then store it and pub it to zmq
         try:
-            hourlyupdated = False
             async with websockets.connect('wss://www.deribit.com/ws/api/v2') as websocket:
                 self.logger.info('Connected to deribit websocket server')
                 
                 # set heartbeats to keep alive
                 await websocket.send(json.dumps(heartbeat))
                 await websocket.recv()
-                # get instruments
-                await websocket.send(json.dumps(instruments))
-                response = json.loads(await websocket.recv())
-                hourlyupdated = True
 
                 # it is very important here to use 'self.state' to control start/stop!!!
                 lastheartbeat = time.time()
@@ -95,27 +91,17 @@ class DeribitMD(ServiceBase):
                     # check heartbeat to see if websocket is broken
                     if time.time() - lastheartbeat > 15:
                         raise websockets.exceptions.ConnectionClosedError(1003, 'Serverside heartbeat stopped.')
-                    
-                    # update instruments every hour
-                    if time.gmtime().tm_min == 5 and hourlyupdated == False:
-                        self.logger.info('Fetching instruments hourly ******')
-                        await websocket.send(json.dumps(instruments))
-                        hourlyupdated = True
-                    elif time.gmtime().tm_min == 31 and hourlyupdated == True:
-                        hourlyupdated = False
-                    else:
-                        pass
-                    
+
+                    # send all requests to deribit
+
+                    # then deal with every received msg
                     response = json.loads(await websocket.recv())
-                    # need response heartbeat to keep alive
+                    
                     if response.get('method', '') == 'heartbeat':
+                        # need response heartbeat to keep alive
                         if response['params']['type'] == 'test_request':
                             lastheartbeat = time.time()
                             await websocket.send(json.dumps(test))
-                        else:
-                            self.logger.info('Serverside heartbeat: ' + str(response))
-                    elif response.get('id', '') == MSG_INSTRUMENTS_ID:
-                        pass # here to update instruments
                     elif response.get('id', '') in (MSG_TEST_ID, ):
                         pass # here to pass useless response
                     else:
@@ -147,8 +133,9 @@ class DeribitMD(ServiceBase):
                 msg.update({'status': 'accepted'})
                 key = '.'.join([msg[i] for i in ('sid', 'internalid', 'exid', 'accountid')])
                 orders[key] = msg
-                send_msg_2deribit
-                msg.update({'status': 'sent'})
+                # send_msg_2deribit_use_account_specific_websocket
+                asyncio.create_task(pub_msg(accountid))
+                # msg.update({'status': 'sent'})
         except Exception as e:
             self.logger.exception(e)
             await self.on_request()
@@ -159,7 +146,8 @@ class DeribitMD(ServiceBase):
             self.logger.error('tried to run service, but state is %s' % self.state)
         else:
             self.state = ServiceState.started
-            await asyncio.gather(self.pub_msg(), self.on_request())
+            # await asyncio.gather(self.pub_msg(), self.on_request())
+            await self.on_request()
 
 
     
